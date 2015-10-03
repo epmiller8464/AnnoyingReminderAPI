ef111cbc156f45ea99e6b2ace8ea9bdfc66e08c2
Eric Miller <epmiller8464@gmail.com>
2015-10-03 07:09:00 -0500

    stream logs

--- app.js
+++ app.js
@@ -69,22 +69,15 @@ var util = require('util');
 
 console.log(util.inspect(process.memoryUsage()));
 
+var ext = config.get('ext'),
+    dirPath = config.get('sourceFilePath'),
+    np = config.get('namePattern');
+
+dirPath = path.normalize(dirPath);
+
 app.initio = function (http) {
-    var ext = config.get('ext');
-    var dirPath = config.get('sourceFilePath');
-    var np = config.get('namePattern');
     io = io(http);
     io.on('connection', function (socket) {
-
-        //fs.readFile('tmp.txt', 'utf8',
-        //    function (err, data) {
-        //        if (err) {
-        //            socket.emit('news', err.message);
-        //
-        //        } else {
-        //            socket.emit('news', data);
-        //        }
-        //    });
         var watchFiles;// = {};
 
         var nameRegex = new RegExp(np);
@@ -111,18 +104,14 @@ app.initio = function (http) {
                             ext: ext,
                             name: f
                         });
-                        var stat = fs.statSync(fp);
                         watchFiles[f] = {fp: fp, stat: undefined, name: f};
                     }
                 });
 
                 if (watchFiles) {
-                    var fstats = {}
                     for (var index in watchFiles) {
                         var file = watchFiles[index];
                         fs.watch(file.fp, {persistent: true, recursive: false}, function (e, fileName) {
-                            //console.log(curr.size,prev.size);
-
                             var lf = watchFiles[fileName];
                             if (!watchFiles[fileName]) {
                                 debug('something is wrong watching the file', fileName);
@@ -131,10 +120,8 @@ app.initio = function (http) {
                             var curr = fs.statSync(lf.fp),
                                 prev = lf.stat;
                             var o = {encoding: 'utf8', start: 0};//, end: curr.size};
-                            if (prev) {
-                                if (curr.size > prev.size) {
-                                    o.start = prev.size;// prev.size == 0 ? prev.size : curr.size - prev.size;
-                                }
+                            if (prev && curr.size > prev.size) {
+                                o.start = prev.size;// prev.size == 0 ? prev.size : curr.size - prev.size;
                             }
                             var rr = fs.createReadStream(lf.fp, o);
                             rr.on('data', function (data) {
@@ -156,107 +143,55 @@ app.initio = function (http) {
                 }
             });
         }
-        //fs.
-        //fs.watchFile('tmp.txt', {persistent: true, interval: 3000}, function (curr, prev) {
-        //    //console.log(curr.size,prev.size);
-        //    if (curr.size > prev.size) {
-        //        //fs.readFile('tmp.txt', 'utf8',
-        //        //    function (err, data) {
-        //        //        if (err) {
-        //        //            socket.emit('news', err.message);
-        //        //
-        //        //        } else {
-        //        //            socket.emit('news', data);
-        //        //
-        //        //        }
-        //        //    });
-        //        var size = prev.size == 0 ? prev.size : curr.size - prev.size;
-        //        var o = {encoding: 'utf8', start: size};//, end: curr.size};
-        //        var rr = fs.createReadStream('tmp.txt', o);
-        //        //rr.on('readable', function () {
-        //        //    console.log('readable:', rr.read());
-        //        //});
-        //        rr.on('data', function (data) {
-        //            console.log('read data:', data.length);
-        //            socket.emit('news', data);
-        //            var mem = process.memoryUsage();
-        //            console.log(util.format('%s kb %s mb', mem.heapTotal / 1e4, mem.heapTotal / 1e6));
-        //            console.log(util.inspect(process.memoryUsage()));
-        //        });
-        //        rr.on('end', function () {
-        //            //        console.log('end');
-        //        });
-        //
-        //        rr.on('close', function () {
-        //            //         console.log('close');
-        //        });
-        //    }
-        //});
     });
 };
 
 var os = require('os');
-(function (c) {
-    //async.nextTick(function () {
-    //    console.log('thread 1 :' + process.pid);
-    //    console.log('thread 1 :' + process.uptime());
-    //});
-
-    //rr.setEncoding('utf8');
-    var wsr = fs.createWriteStream('tmp.txt', {encoding: 'utf8'});
-    wsr.on('finish', function () {
-        console.error('all writes are now complete.');
-    });
-    wsr.once('drain', function (x) {
-        //   console.log('draining');
-        l = 0;
-    });
+(function () {
+    var dSize = fs.statSync('data.json').size;
     var offset = 0;
     var l = 0;
-    // wsr.cork();
     setInterval(function () {
+        offset = offset < dSize ? offset : 0;
+        var stop = Math.floor((Math.random() * 20) + 1);
+        var o = {encoding: 'utf8', start: offset, end: offset + stop};
 
-        var stats = fs.statSync('tmp.txt');
-        //fs.read('tmp.txt', b, offset, l, p, function (e, br, buf) {
-        //    console.log('read: ' + br, buf.length);
-        //
-        //});
-        //rr.read();
-
-        var log = 'pid: ' + process.pid + ' t: ' + process.uptime() + '';
-        //wsr.write(log, 'utf8', function () {
-        //    //l = stats.size / 1024;
-        //    console.log('stats', stats.size / 1e4);
-        //});
-        //console.log(log);
-        fs.appendFile('tmp.txt', log, 'utf8', function (err) {
-            if (err) {
-                console.log(err.message);
-            }
+        var path = dirPath + "media-server_2015-10-02_15-01-36.00001.pid1164.log";
+        var wsr = fs.createWriteStream(path, {encoding: 'utf8'});
+        wsr.on('finish', function () {
+            //  console.error('all writes are now complete.');
         });
-        //var i = b.write(log, offset, log.length, 'utf8');
-        //offset += i;
-        //console.log(log.length);
-        //console.log(Buffer.byteLength(log, 'utf8'));
-        //if ((offset + Buffer.byteLength(log)) >= (b.length + 1)) {
-        //    stats = fs.statSync('tmp.txt');
-        //
-        //    wsr.write(b, 'utf8', function () {
-        //        console.log('stats', stats.size);
-        //    });
-        //    b = new Buffer(256);
-        //    // wsr.end('this is the end\n');
-        //    //  wsr.uncork();
-        //    //wsr.cork();
-        //    offset = l = 0;
-        //}
-        //
-        //l += 1;
+        wsr.once('drain', function (x) {
+        });
 
-    }, 500);
+        var rr = fs.createReadStream('data.json', o);
+        rr.pipe(wsr);
+        //rr.on('readable', function () {
+        //    var vr = rr.read(1);
+        //    offset = null == vr ? offset = 0 : offset;
+        //});
+        //rr.on('data', function (data) {
+        //    console.log('write data:', data);
+        //    //wsr.write(data);
+        //    //fs.appendFile(path, data, 'utf8', function (err) {
+        //    //    if (err) {
+        //    //        console.log(err.message);
+        //    //    }
+        //    //});
+        //});
+        //rr.on('end', function () {
+        //});
+        //
+        //rr.on('close', function () {
+        //});
 
-})(config);
-//(function setup(c){
-//}(config));
+
+        offset += stop + 1;// Math.floor((Math.random() * 20) + 1);
+
+
+    }, Math.floor((Math.random() * 3000) + 1));
+    //}, 0);
+
+})();
 
 module.exports = app;
